35d9d3081e0dccdc5b6ba130c2d1d26a6b9a3713
张雪东 <zhangxuedong@zhangxuedongdeimac.local>
2016-01-07 09:21:25 +0800

    Initial Commit

---
+++ UIBeizerPathDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIBeizerPathDemo
+//
+//  Created by 张雪东 on 16/1/7.
+//  Copyright © 2016年 张雪东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
